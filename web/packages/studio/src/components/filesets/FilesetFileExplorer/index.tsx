d90ad406d65153f1bfdf2a777cd15fbb1578e517
Sean Teramae <steramae@nvidia.com>
2026-06-03 12:36:44 -0700

    fix height
    Signed-off-by: Sean Teramae <steramae@nvidia.com>

--- web/packages/studio/src/components/filesets/FilesetFileExplorer/index.tsx
+++ web/packages/studio/src/components/filesets/FilesetFileExplorer/index.tsx
@@ -440,7 +440,7 @@ export const FilesetFileExplorer: FC<FilesetFileExplorerProps> = ({
             </Flex>
           ) : (
             <>
-              <Stack gap="density-md" className="h-full min-h-0 flex flex-col">
+              <Stack gap="density-md" className="max-h-full min-h-0 flex flex-col">
                 <TableToolbar
                   aria-label="Dataset files toolbar"
                   className="min-w-0 shrink-0"
@@ -599,7 +599,10 @@ export const FilesetFileExplorer: FC<FilesetFileExplorerProps> = ({
                     />
                   </Flex>
                 ) : (
-                  <Flex className="w-full min-h-0 flex-1 overflow-auto border-base border-1 rounded-lg">
+                  <Flex
+                    align="start"
+                    className="w-full min-h-0 flex-1 overflow-auto border-base border-1 rounded-lg"
+                  >
                     <Table className="w-full" columns={columns} rows={rows} />
                   </Flex>
                 )}
