4320d58087f249d3696f34098a1ea951ed9b95e3
Mike Knepper <mknepper@nvidia.com>
2026-07-27 09:06:14 -0500

    Update some comments
    Signed-off-by: Mike Knepper <mknepper@nvidia.com>

--- packages/data_designer_nemo/src/data_designer_nemo/filesystem.py
+++ packages/data_designer_nemo/src/data_designer_nemo/filesystem.py
@@ -7,14 +7,11 @@ from nemo_platform.filesets import FilesetFileSystem
 from nemo_platform_plugin.client.adapter import client_from_platform
 from nemo_platform_plugin.files.client import FilesClient
 
-# TODO: Update the FilesetFileSystem constructor to accept an explicit `asynchronous: bool | None`
-# argument so that the fsspec asynchronous mode is not coupled to the `client`. Once that exists,
-# we can drop `_get_files_client` and `_async_to_sync_sdk`, leaving us with something like:
-#
-# FilesetFileSystem(async_client=_get_async_files_client(sdk), asynchronous=False)
-
 
 def make_filesystem(sdk: NeMoPlatform | AsyncNeMoPlatform) -> FilesetFileSystem:
+    # We MUST pass a sync FilesClient here: it forces fsspec into synchronous mode
+    # (necessary because the upstream DD library calls sync fsspec APIs) AND it
+    # avoids fsspec trying to reuse an AsyncNeMoPlatform on a different event loop
     return FilesetFileSystem(client=_get_files_client(sdk))
 
 
