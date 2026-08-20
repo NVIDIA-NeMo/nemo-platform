dc9dcba8c8ce44c0ae1e9cb3a160c4b7ef1c3417
Matt Kornfield <mkornfield@nvidia.com>
2026-08-20 11:17:26 -0700

    chore: copyright
    Signed-off-by: Matt Kornfield <mkornfield@nvidia.com>

--- e2e/k8s/scripts/run_network_policy_e2e.sh
+++ e2e/k8s/scripts/run_network_policy_e2e.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 #
 # Local end-to-end path for Helm NetworkPolicy enforcement.
 
